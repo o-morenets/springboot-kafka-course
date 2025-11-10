127514ee35ec8c74e80b21738c5fe96819f133de
Oleksii Morenets <oleksii.morenets@gmail.com>
2025-11-10 21:32:15 +0100

    WikimediaChangesProducer.java removed

--- springboot-kafka-wikimedia/kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/SpringBootProducerApplication.java
+++ springboot-kafka-wikimedia/kafka-producer-wikimedia/src/main/java/net/javaguides/springboot/SpringBootProducerApplication.java
@@ -1,25 +1,14 @@
 package net.javaguides.springboot;
 
 import lombok.RequiredArgsConstructor;
-import net.javaguides.springboot.event.WikimediaChangesProducer;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class SpringBootProducerApplication implements CommandLineRunner {
+public class SpringBootProducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
-    }
-
-    private final WikimediaChangesProducer wikimediaChangesProducer;
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        // Uncomment the following line if you want a fire-and-forget 10-minutes job that runs on startup:
-//        wikimediaChangesProducer.startPublishing();
     }
 }
